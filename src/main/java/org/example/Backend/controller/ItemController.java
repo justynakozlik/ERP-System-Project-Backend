f77ccb8d40390d3e225e9f81bb565031b1b4e617
Justyna Koźlik <jkozlik96@gmail.com>
2021-01-18 13:58:41 +0100

    * Retrieve items from the database

--- src/main/java/org/example/Backend/controller/ItemController.java
+++ src/main/java/org/example/Backend/controller/ItemController.java
@@ -1,12 +1,14 @@
 package org.example.backend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.dto.ItemDto;
 import org.example.backend.entity.Item;
 import org.example.backend.repository.ItemRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,8 +22,11 @@ public class ItemController {
     }
 
     @GetMapping("/items")
-    public List<Item> listItems() {
-        return itemRepository.findAll();
+    public List<ItemDto> listItems() {
+        return itemRepository.findAll()
+                .stream()
+                .map(ItemDto::of)
+                .collect(Collectors.toList());
     }
 
     @DeleteMapping("/items")

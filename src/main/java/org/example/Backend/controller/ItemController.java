bc91067affa307bd81c1d70bb26c033766a9e567
Justyna Koźlik <jkozlik96@gmail.com>
2021-01-21 15:14:10 +0100

    * Updating item data

--- src/main/java/org/example/Backend/controller/ItemController.java
+++ src/main/java/org/example/Backend/controller/ItemController.java
@@ -3,10 +3,12 @@ package org.example.backend.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.EmployeeDto;
 import org.example.backend.dto.ItemDto;
+import org.example.backend.dto.ItemEditViewDto;
 import org.example.backend.dto.ItemSavedDto;
 import org.example.backend.entity.Employee;
 import org.example.backend.entity.Item;
 import org.example.backend.repository.ItemRepository;
+import org.example.backend.repository.QuantityTypeRepository;
 import org.example.backend.service.ItemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +23,19 @@ public class ItemController {
 
     private final ItemRepository itemRepository;
     private final ItemService itemService;
+    private final QuantityTypeRepository quantityTypeRepository;
 
     @PostMapping("/items")
     public ItemDto newItem(@RequestBody ItemSavedDto dto) {
-        return ItemDto.of(itemService.saveItem(dto));
+        if (dto.getIdItem() == null) {
+            return ItemDto.of(itemService.saveItem(dto));
+        } else {
+            Item item = itemRepository.findById(dto.getIdItem()).get();
+            item.setName(dto.getName());
+            item.setQuantity(dto.getQuantity());
+            item.setQuantityType(quantityTypeRepository.findById(dto.getIdQuantityType()).get());
+            return ItemDto.of(itemRepository.save(item));
+        }
     }
 
     @GetMapping("/items")
@@ -40,6 +51,13 @@ public class ItemController {
         Thread.sleep(500);
         Optional<Item> optionalItem = itemRepository.findById(idItem);
         return ItemDto.of(optionalItem.get());
+    }
+
+    @GetMapping("/item_edit_data/{idItem}")
+    public ItemEditViewDto getItemEditViewDto(@PathVariable Long idItem) {
+        Item item = itemRepository.findById(idItem).get();
+        ItemEditViewDto dto = ItemEditViewDto.of(item, quantityTypeRepository.findAll());
+        return dto;
     }
 
     @DeleteMapping("/items")
--- src/main/java/org/example/Backend/dto/ItemSavedDto.java
+++ src/main/java/org/example/Backend/dto/ItemSavedDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class ItemSavedDto {
 
+    private Long idItem;
     private String name;
     private Double quantity;
     private Long idQuantityType;
